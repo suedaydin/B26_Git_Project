1eb2a7945139887163afd75039d937e97500d46a
suedaydin <suedaydn@gmail.com>
2022-03-26 16:11:51 -0400

    Revert "updated First file"
    This reverts commit 2165e63e8bf2d0437768d6be30dea06dd76f0c57.

--- src/day0/First.java
+++ src/day0/First.java
@@ -7,9 +7,6 @@ public class First {
         // TODO: Works on this tomorrow
         //  add some cool code
 
-        System.out.println("java");
-        System.out.println("Working...");
-
         // extra line 1
         // extra line 2
         // extra line 3
